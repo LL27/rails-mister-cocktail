3b353a2b120e3a9bba8a4825dac44dbeb33d7ede
Lital Levin <lital.levin@gmail.com>
2020-02-14 16:33:25 +0100

    add background image to cocktail cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
 
-Cocktail.create(name: "Moloko Plus")
-Cocktail.create(name: "Long Island Iced Tea")
+
+file = URI.open('https://assets.afcdn.com/recipe/20180801/81649_w648h344c1cx1766cy2888.jpg')
+ramos = Cocktail.create!(name: "Crystal Ramos Gin Fizz")
+
+ramos.photo.attach(io: file, filename: 'ramos.jpg', content_type: 'image/jpg')
+
+a = Ingredient.create(name: "Tanqueray gin")
+b = Ingredient.create(name: "Clarified gin milk punch")
+c = Ingredient.create(name: "Orange blossom bubbles")
+
+
+Dose.create(
+  description: "1 1/4 cup",
+  ingredient_id: a.id,
+  cocktail_id: ramos.id
+)
+
+Dose.create(
+  description: "6 3/4 cup",
+   ingredient_id: b.id,
+  cocktail_id: ramos.id
+)
+
+Dose.create(
+  description: "Garnish",
+   ingredient_id: c.id,
+  cocktail_id: ramos.id
+)
