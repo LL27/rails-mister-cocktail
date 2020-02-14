b48b12c43555fbe20691c4ffbfb5cb1c64e27d48
Lital Levin <lital.levin@gmail.com>
2020-02-14 12:01:52 +0100

    destroy

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,7 +20,12 @@ class DosesController < ApplicationController
 
   def destroy
 
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
+
 
   private
 
