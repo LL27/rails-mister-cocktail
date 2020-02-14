802b5d975136b106abf7189acae8e5efca83f255
Lital Levin <lital.levin@gmail.com>
2020-02-14 13:16:40 +0100

    attach photo to model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,6 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
+
 end
